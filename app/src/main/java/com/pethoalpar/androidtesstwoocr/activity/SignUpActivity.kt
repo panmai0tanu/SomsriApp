06eb4d0b02fb396426d03acf255554bad48dce89
panmai0tanu <panmai_tanu@cmu.ac.th>
2019-03-05 19:22:45 +0700

    create database user

--- app/src/main/java/com/pethoalpar/androidtesstwoocr/activity/SignUpActivity.kt
+++ app/src/main/java/com/pethoalpar/androidtesstwoocr/activity/SignUpActivity.kt
@@ -5,19 +5,21 @@ import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
 import android.util.Log
 import android.view.View
+import com.pethoalpar.androidtesstwoocr.MainApp
 import com.pethoalpar.androidtesstwoocr.R
 import com.pethoalpar.androidtesstwoocr.ToolbarActivity
 import com.pethoalpar.androidtesstwoocr.model.constructorUser
 import com.pethoalpar.androidtesstwoocr.room.ItemDao
 import com.pethoalpar.androidtesstwoocr.room.UserDao
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import java.util.*
 import javax.inject.Inject
 
 
 class SignUpActivity : ToolbarActivity() {
 
     @Inject
-    lateinit var itemDao: ItemDao
+    lateinit var userDao: UserDao
 
     private var email: String = ""
     private var pass: String = ""
@@ -26,6 +28,7 @@ class SignUpActivity : ToolbarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        MainApp.graph.inject(this)
 
         tv_signup.setOnClickListener {
 
@@ -35,13 +38,14 @@ class SignUpActivity : ToolbarActivity() {
 
             if (validatePassword() && isValidEmail(email)) {
                 val user = constructorUser()
-                user.userId = 0
+                while (userDao.findByUserId(user.userId!!).isNotEmpty())
+                    user.userId = (0..1000).random()
                 user.userEmail = email
                 user.password = pass
 
-//                userDao.create(user)
+                userDao.create(user)
 
-                Log.d("PANMAI", itemDao.all().toString())
+                Log.d("PANMAI", userDao.all().toString())
 
             } else {
                 Log.d("PANMAI", et_sign_up_email.text.toString() + ", false")
@@ -52,13 +56,15 @@ class SignUpActivity : ToolbarActivity() {
 
     fun validatePassword(): Boolean {
 
+        tv_error_mess.visibility = View.GONE
+
         return if (!conPass.equals(pass)) {
             tv_error_mess.visibility = View.VISIBLE
-            tv_error_mess.setText("Password Not matching")
+            tv_error_mess.setText("Password Not matching.")
             false
         } else if (pass.length < 6) {
             tv_error_mess.visibility = View.VISIBLE
-            tv_error_mess.setText("Password Not matching")
+            tv_error_mess.setText("Password less six character.")
             false
         } else
             true
@@ -66,10 +72,15 @@ class SignUpActivity : ToolbarActivity() {
 
     fun isValidEmail(target: String): Boolean {
         return if (TextUtils.isEmpty(target)) {
+            tv_error_mess.visibility = View.VISIBLE
+            tv_error_mess.setText("Not match case email.")
             false
         } else {
             android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches()
         }
     }
+
+    fun IntRange.random() =
+            Random().nextInt((endInclusive + 1) - start) + start
 }
 
